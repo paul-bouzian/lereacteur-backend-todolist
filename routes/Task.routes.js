944e73ce685f411147ba317d252a2e93a04cf642
Paul Bouzian <paulbouzian@mbp-de-paul.home>
2024-06-10 02:43:48 +0200

    added put route

--- routes/Task.routes.js
+++ routes/Task.routes.js
@@ -39,4 +39,22 @@ router.get("/tasks", async (req, res) => {
   }
 });
 
+router.put("/tasks/:text", async (req, res) => {
+  try {
+    const { text } = req.params;
+    const { completed } = req.body;
+    const task = await Task.findOne({ text: text });
+
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    task.completed = completed;
+    await task.save();
+    return res.status(200).json(task);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
