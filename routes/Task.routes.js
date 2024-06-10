ad9a4a07e077ac5a2cd1d43c73618c545caec375
Paul Bouzian <paulbouzian@mbp-de-paul.home>
2024-06-10 02:36:53 +0200

    fixed delte route

--- routes/Task.routes.js
+++ routes/Task.routes.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Task = require('../models/Task');
+const Task = require("../models/Task");
 
-router.post('/tasks', async (req, res) => {
-
+router.post("/tasks", async (req, res) => {
   try {
     const { text, completed } = req.body;
     const task = new Task({ text, completed });
@@ -15,25 +14,23 @@ router.post('/tasks', async (req, res) => {
   }
 });
 
-router.delete('/tasks/:id', async (req, res) => {
-  
-    try {
-      const { id } = req.params;
-      const task = await Task.findById(id);
+router.delete("/tasks/:text", async (req, res) => {
+  try {
+    const { text } = req.params;
+    const task = await Task.findOne({ text: text });
 
-      if (!task) {
-        return res.status(404).json({ error: 'Task not found' });
-      }
-
-      await task.deleteOne();
-      return res.status(204).send();
-    } catch (error) {
-      return res.status(400).json({ error: error.message });
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
     }
+
+    await task.deleteOne();
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 });
 
-router.get('/tasks', async (req, res) => {
-
+router.get("/tasks", async (req, res) => {
   try {
     const tasks = await Task.find();
     return res.status(200).json(tasks);
@@ -42,4 +39,4 @@ router.get('/tasks', async (req, res) => {
   }
 });
 
-module .exports = router;
+module.exports = router;
